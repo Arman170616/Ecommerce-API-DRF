366c850072c3873d8de07eb5dbc622045666da21
Arman170616 <armanice09@gmail.com>
2023-09-12 09:18:05 +0600

    api_product_views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import *
+from .models import *
+from rest_framework import status
 
-# Create your views here.
+
+@api_view(['GET'])
+
+def api_products(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
