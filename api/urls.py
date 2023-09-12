853fab45f455b44f0b5f8dad091b5aa308713874
Arman170616 <armanice09@gmail.com>
2023-09-12 09:48:52 +0600

    single product

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('products/', api_products),
-    # path('products/<str:pk>/', api_product),
+    path('products/<str:pk>/', api_product),
 ]
+
+# 1bbe17f8-1f45-463b-9337-817254e295c3
--- api/views.py
+++ api/views.py
@@ -12,3 +12,10 @@ def api_products(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
+
+
+@api_view()
+def api_product(request, pk):
+    product = Product.objects.get(id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
